6c0bcbc11032e278f47583296823ecbab5330248
Helton Moraes <heltonbiker@gmail.com>
2017-06-20 22:40:07 -0300

    WPF first commit - very rough sketch of application

---
+++ WPF/MapComplete/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MapComplete
+{
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var window = new MapCompleteWindow()
+			{
+				DataContext = new MapCompleteViewModel()
+			};
+
+			MainWindow = window;
+
+			MainWindow.Show();
+		}
+	}
+}
